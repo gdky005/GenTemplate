37e33864419d2af5eed96369e494a1af63134fcf
WangQing <wangqing11@100tal.com>
2019-06-28 18:48:37 +0800

    1. 添加工程包文件到项目中； 2. 把资源文件解压到相关目录； 3. 可以生成目录文件； 4. 目前只能拷贝相关文件到本地，还有 test.html 做了替换，其他的暂无。

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,48 @@
+
 import bean.TestBean
 import freemarker.template.Configuration
 import freemarker.template.TemplateExceptionHandler
-import java.io.*
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FilenameUtils
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
 fun main(args: Array<String>) {
     logD("开始生成模板：")
 
-    val pro_dir = System.getProperty("user.dir")
-    val template_dir = "/src/main/resources/"
+    val proDir = System.getProperty("user.dir")
+    val templateSourceDir = "/src/main/resources/"
     val encode = "UTF-8"
     val resourceFtl = "/test.html.ftl"
-    val templateDir = "$pro_dir/ftl"
-    var testFtl = template_dir + resourceFtl
-    var destFilePath = "$pro_dir/ftl$resourceFtl"
+    val resourceZip = "/templateZip.zip"
 
-    val name = getFileNameNoFTL(testFtl)
-    val distFile = File("$pro_dir/$name")
-    logD("根目录是: $pro_dir")
+    val templateOutDir = "$proDir/out"
+    val templateDestFtlDir = "$templateOutDir/ftl"
+    val testFtl = templateSourceDir + resourceFtl //test.ftl 文件目录
 
+    val name = FilenameUtils.getBaseName(testFtl)
+    val distTestFile = File("$templateDestFtlDir/$resourceFtl")
+    val destTestFile = File("$templateOutDir/$name")
+
+    logD("根目录是: $proDir")
     try {
-        val input = Thread.currentThread().javaClass.getResourceAsStream(resourceFtl)
-        writeToLocal(destFilePath, input)
+        val javaCls = Thread.currentThread().javaClass
 
-        testFtl = "$pro_dir/$name"
+        val testInput = javaCls.getResourceAsStream(resourceFtl)
+        FileUtils.copyInputStreamToFile(testInput, distTestFile)
+
+        val inputZip = javaCls.getResourceAsStream(resourceZip)
+        FileUtils.copyInputStreamToFile(inputZip, File(templateOutDir, resourceZip))
     } catch (e: Exception) {
         e.printStackTrace()
     }
 
+    Thread.sleep(500)
+
     try {
         val cfg = Configuration(Configuration.VERSION_2_3_22)
-        cfg.setDirectoryForTemplateLoading(File(templateDir))
+        cfg.setDirectoryForTemplateLoading(File(templateDestFtlDir))
         cfg.defaultEncoding = encode
         cfg.templateExceptionHandler = TemplateExceptionHandler.RETHROW_HANDLER
 
@@ -37,13 +50,17 @@ fun main(args: Array<String>) {
                 "周杰伦",
                 "我是你的老歌迷了")
 
-        logD("模板文件地址：$destFilePath")
+        logD("模板文件地址：$templateDestFtlDir")
         val template = cfg.getTemplate(resourceFtl)
-        val writer = FileWriter(distFile)
+        val writer = FileWriter(destTestFile)
         template.process(testBean, writer)
         writer.close()
 
-        logD("生成的文件：" + distFile.path)
+        logD("生成的文件：" + distTestFile.path)
+        logD("开始解压文件中...")
+        UnZip.unZip(File(templateOutDir, resourceZip), templateOutDir + "/aaa")
+
+        logD("解压文件 完成")
 
     } catch (e: IOException) {
         e.printStackTrace()
@@ -54,68 +71,6 @@ fun main(args: Array<String>) {
     logD("生成模板完成！")
 }
 
-
-/**
- * 根据文件名字，去除 ftl
- * @param testFtl   文件中字符串
- * @return  返回去除 ftl 的文件名
- */
-private fun getFileNameNoFTL(testFtl: String): String {
-    return File(testFtl).name.replace(".ftl", "")
-}
-
-
 private fun logD(msg: String) {
     println(msg)
 }
-
-/**
- * 将InputStream写入本地文件
- * @param destination 写入本地目录
- * @param input    输入流
- * @throws IOException
- */
-private fun writeToLocal(destination: String, input: InputStream) {
-    try {
-        val file = File(destination)
-        if (!file.parentFile.exists()) {
-            file.parentFile.mkdirs()
-        }
-
-        if (!file.exists()) {
-            file.createNewFile()
-
-            val downloadFile = FileOutputStream(destination)
-
-            someFunc(input, downloadFile)
-
-            downloadFile.close()
-            input.close()
-        } else {
-            logD("无需创建文件，已经使用本地缓存文件：" + file.absolutePath)
-        }
-
-
-    } catch (e: Exception) {
-    }
-}
-
-
-/**
- * 把 InputStream 流 写入到 OutputStream 中。
- */
-fun someFunc(input: InputStream, output: OutputStream) {
-    try {
-        var read: Int = -1
-        input.use { inputStream ->
-            output.use {
-                while (inputStream.read().also { read = it } != -1) {
-                    it.write(read)
-                }
-            }
-        }
-    } catch (t: Throwable) {
-        t.printStackTrace()
-    }
-}
-
